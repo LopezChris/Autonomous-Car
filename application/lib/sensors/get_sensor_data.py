72bb1a0ff439951977cd1b6959fea6aa753ba1d0
Your Name <you@example.com>
2019-01-29 20:12:54 -0800

    Added capturing camera frames and the other sensors, their filenames are stored in a csv with each frame captured

--- application/lib/sensors/get_sensor_data.py
+++ application/lib/sensors/get_sensor_data.py
@@ -1,33 +1,87 @@
 #!/usr/bin/env python
 
-import rospy
-from std_msgs.msg import String, Header, Float64
-import numpy as np
-from threading import Thread
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
-from sensor_msgs.msg import LaserScan, Imu
-from scipy import signal, stats
-import matplotlib.pyplot as plt
-import math
 from geometry_msgs.msg import Polygon, Point32, PolygonStamped
-import time
+from std_msgs.msg import String, Header, Float64
+from sensor_msgs.msg import LaserScan, Imu, Image
+from cv_bridge import CvBridge, CvBridgeError
 from utils import pre_process_lidar_data
+from scipy import signal, stats
+from threading import Thread
+
+import matplotlib.pyplot as plt
+import sensor_msgs.msg
+import numpy as np
+import cStringIO
+import roslib
+import rospy
+import uuid
+import math
+import time
 import csv
 import sys
+import cv2
 import os
 
 ##
 #
-# Ingest data from all sensors connected to MIT Racecar
+# Ingest data from all sensors connected to Racecar
 #
 ##
 
-class CarSensors():
-    def __init__(self):
-        # base_dir = "/media/nvidia/6139-3863/car_sensors"
-        base_dir = "/home/nvidia/Autonomous-Car/application/data"
-        self.sensor_dir = [base_dir+"/lidar_scan/", base_dir+"/imu_data/", base_dir+"/motor_speed/",
-                           base_dir+"/servo_steering/"]
+FRAME_RATE = 30
+USE_LOGIC_CAMERA = 1
+
+class LogCarSensors():
+    def __init__(self, save_base_dst):
+        # Store Data in sensor_dir
+        self.base_dir = save_base_dst
+        # self.base_dir = "/media/nvidia/6139-3863/data/track"
+        # self.base_dir = "/home/nvidia/Autonomous-Car/application/data/track"
+        self.sensor_dir = [self.base_dir+"/lidar_scan/", self.base_dir+"/imu_data/", 
+                           self.base_dir+"/motor_speed/", self.base_dir+"/servo_steering/", 
+                           self.base_dir+"/cam_frames/"]
+
+        # Initialize data recording
+        self.record = 0
+
+        # Initialize sensor data attributes
+        self.lidar_raw_data = -1
+        self.imu_raw_data = -1
+        self.speed_raw_data = -1
+        self.steering_raw_data = -1
+        self.camera_raw_np_img = []
+
+        self.last_time_stamp = -1
+
+        # Camera sensor, check which topic to use
+        if USE_LOGIC_CAMERA:
+            # Subscribe to Cam "/webcam/image_raw/" topic with data published by the "/webcam_node",
+            # In terminal, run ROS command to see camera data: "rostopic echo "/webcam/image_raw/"
+            camera_topic = "/webcam/image_raw/"
+        else:
+            left_camera_raw_topic = "/zed/left/image_raw_color"
+            left_camera_rect_topic = "/zed/left/image_rect_color"
+            right_camera_raw_topic = "/zed/right/image_raw_color"
+            right_camera_rect_topic = "/zed/right/image_rect_color"
+
+        # Camera sensor, check which topic to use
+        if USE_LOGIC_CAMERA:
+            # Subscribe to Cam "/webcam/image_raw/" topic with data published by the "/webcam_node",
+            # In terminal, run ROS command to see camera data: "rostopic echo "/webcam/image_raw/"
+            self.sub_camera = rospy.Subscriber(camera_topic, Image, self.camera_callback)
+        else:
+            self.sub_camera = rospy.Subscriber(right_camera_rect_topic, Image, self.zos_camera_callback)
+
+        # Check if sensor directories do not exist, if true, then create them
+        for dir_name in self.sensor_dir:
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+                rospy.loginfo("Created: %s", dir_name)
+
+        # Subscribe to Joy "/joy" topic with data being published by the "/joy_node",
+        # In terminal, run ROS command to see joy data: "rostopic echo "/joy"
+        self.sub_joy = rospy.Subscriber("/joy", sensor_msgs.msg.Joy, self.joy_callback)
 
         # Subscribe to LiDAR "/scan" topic with data being published by the "/laser_node", 
         # call lidar_callback method when data takes up 1 space in queue
@@ -50,52 +104,155 @@ class CarSensors():
         self.sub_esc_steering = rospy.Subscriber("/vesc/commands/servo/position", Float64,
                                                  self.steering_callback, queue_size = 1)
 
-        # TODO: Need to subscribe to battery sensor and Camera sensor
+        # TODO: Need to subscribe to battery sensor
+        
+        # Bridge for converting ROS Image to CV2 Image
+        self.bridge = CvBridge()
 
+        # Store filenames for sensor data into csv file
+        csv_file = open(self.base_dir + "/sensor_data.csv", "ab")
 
-        # Check if sensor directories do not exist, if true, then create them
-        for dirname in self.sensor_dir:
-            if not os.path.exists(dirname):
-                os.makedirs(dirname)
-                rospy.loginfo("Created: %s", dirname)
+        self.csv_writer = csv.writer(csv_file, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+		
+        self.csv_writer.writerow(["camera_frame", "lidar_scan", "esc_speed",
+                                  "esc_steering", "imu"])
+
+    def joy_callback(self, joy_raw_msg):
+        """Sets ON or OFF flag for recording data from all sensors.
+           When B is pressed, data recording is off, but if Y is pressed, data recording on.
+        """
+        if joy_raw_msg.buttons[2] == 1: # B button
+            self.record = 0
+            rospy.logerr("Data Recording Off! %s", str(self.record))
+        if joy_raw_msg.buttons[3] == 1: # Y button
+            self.record = 1
+            rospy.logerr("Data Recording On! %s", str(self.record))
 
     def lidar_callback(self, lidar_raw_msg):
         """Write raw LiDAR data to txt file:
            Data can be used for path planning, object segmentation, obstacle avoidance
         """
-        lidar_raw_file = self.sensor_dir[0] + "lidar-" + time.strftime("%H-%M-%S") + ".txt"
-        
-        with open(lidar_raw_file, "w") as file: # Use file to refer to file object
-            file.write(str(lidar_raw_msg))
+        # Check if data recording on
+        if self.record != 1:
+            return
+
+        self.lidar_raw_data = lidar_raw_msg
 
     def imu_callback(self, imu_raw_msg):
         """
            Write raw IMU data to txt file:
            Data can be used for linear acceleration, angular rotation velocity, magnetic field vectors
         """
-        imu_raw_file = self.sensor_dir[1] + "imu-" + time.strftime("%H-%M-%S") + ".txt"
-        with open(imu_raw_file, "w") as file: # Use file to refer to file object
-            file.write(str(imu_raw_msg))
+        # Check if data recording on
+        if self.record != 1:
+            return
+
+        self.imu_raw_data = imu_raw_msg
 
     def speed_callback(self, speed_raw_msg):
         """
            Write raw speed data to txt file:
            Data can be used for tracking motor speed
         """
-        speed_raw_file = self.sensor_dir[2] + "speed-" + time.strftime("%H-%M-%S") + ".txt"
-        with open(speed_raw_file, "w") as file:
-            file.write(str(speed_raw_msg))
+        # Check if data recording on
+        if self.record != 1:
+            return
+
+        self.speed_raw_data = speed_raw_msg
 
     def steering_callback(self, steering_raw_msg):
-         """
-            Write raw steering data to txt file:
-            Data can be used for tracking servo steering angle
-         """
-         steering_raw_file = self.sensor_dir[3] + "steering-" + time.strftime("%H-%M-%S") + ".txt"
-         with open(steering_raw_file, "w") as file:
-             file.write(str(steering_raw_msg))
+        """
+           Write raw steering data to txt file:
+           Data can be used for tracking servo steering angle
+        """
+        # Check if data recording on
+        if self.record != 1:
+            return
+
+        self.steering_raw_data = steering_raw_msg
+
+    def camera_callback(self, ros_image_msg):
+        """
+           Store camera frame to jpeg file:
+           Data can be used for detecting lane lines, lane curvature, etc
+        """
+        # Check if data recording on
+        if self.record != 1:
+            return
+
+        # Get current time before saving frame
+        current_time = long(time.time() * 1000.0)
+        print("current_time=%f; last_timestamp=%f" %(current_time, self.last_time_stamp))
+        # Check if ms to capture next frame has passed, if not, then exit function
+        if current_time - self.last_time_stamp < 1000 / FRAME_RATE:
+            return
+        # Assign last_time_stamp with new timestamp since we are ready to save another frame
+        self.last_time_stamp = current_time
+
+        # Use cv_bridge() to convert the ROS image to OpenCV format
+        try:
+            frame = self.bridge.imgmsg_to_cv2(ros_image_msg, "bgr8")
+        except CvBridgeError, e:
+            print(e)
+
+        # Convert image to Numpy array, most cv2 functions require Numpy arrays
+        np_img = np.array(frame, dtype = np.uint8)
+
+        # Convert camera_raw_data from list to numpy array
+        self.camera_raw_np_img = np_img
+
+        # Based on camera frames that are captured, save the sensor data recorded per frame
+        self.save_sensor_data()
+
+    def save_sensor_data(self):
+        """
+           Save car's sensor data into csv
+        """
+        # Saved np_img frame with cam_write_file value
+        cam_raw_file = self.sensor_dir[4] + "cam-" + time.strftime("%H-%M-%S") + "-" + str(uuid.uuid4()) + ".jpeg"
+
+        res_saved_file = cv2.imwrite(cam_raw_file, self.camera_raw_np_img)
+
+        # Save Raw LiDAR data at filepath
+        lidar_raw_file = self.sensor_dir[0] + "lidar-" + time.strftime("%H-%M-%S") + "-" + str(uuid.uuid4()) + ".txt"
+
+        with open(lidar_raw_file, "w") as file: # Use file to refer to file object
+            file.write(str(self.lidar_raw_data))
+
+        # Save Raw Speed data at filepath
+        speed_raw_file = self.sensor_dir[2] + "speed-" + time.strftime("%H-%M-%S") + "-" + str(uuid.uuid4()) + ".txt"
+
+        with open(speed_raw_file, "w") as file:
+            file.write(str(self.speed_raw_data))
+
+        # Save Raw Steering data at filepath
+        steering_raw_file = self.sensor_dir[3] + "steering-" + time.strftime("%H-%M-%S") + "-" + str(uuid.uuid4()) + ".txt"
+
+        with open(steering_raw_file, "w") as file:
+            file.write(str(self.steering_raw_data))
+
+        # Save Raw IMU data at filepath
+        imu_raw_file = self.sensor_dir[1] + "imu-" + time.strftime("%H-%M-%S") + "-" + str(uuid.uuid4()) + ".txt"
+
+        with open(imu_raw_file, "w") as file: # Use file to refer to file object
+            file.write(str(self.imu_raw_data))
+
+        # check if path doesn't exist, then return
+        #sensor_filename = []
+        #if not os.path.exists(sensor_filename):
+        #    rospy.logerr("sensor filepath doesn't exist: %s", sensor_filename)
+        #    return  
+
+        self.csv_writer.writerow([cam_raw_file, lidar_raw_file, speed_raw_file,
+                                  steering_raw_file, imu_raw_file])
+
+        rospy.logerr("============ Recording All Sensor Data Per Frame =============")
 
 if __name__=="__main__":
-    rospy.init_node("get_car_data")
-    CarSensors()
+    save_base_dst = "/media/nvidia/6139-3863/log_car_sensors/clockwise_rightside_tracklane"
+    #save_base_dst = "/media/nvidia/6139-3863/log_car_sensors/counterclockwise_rightside_tracklane"
+    #save_base_dst = "/media/nvidia/6139-3863/log_car_sensors/clockwise_leftside_tracklane"
+    #save_base_dst = "/media/nvidia/6139-3863/log_car_sensors/counterclockwise_leftside_tracklane"
+    rospy.init_node("log_car_sensors")
+    LogCarSensors(save_base_dst)
     rospy.spin()
